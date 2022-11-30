5e59823f06ec623863ad9a6bb0054306a01fd678
antonharder1 <anto1388@edu.zealand.dk>
2022-11-30 14:03:09 +0100

    Lmao did API stuffs

---
+++ SysUdvikling2022/AbsenseApi/AbsenseApi/Controllers/AbsenseApiController.cs
@@ -0,0 +1,67 @@
+﻿using AbsenseApi.Managers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentLibrary;
+
+namespace AbsenseApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AbsenseApiController : ControllerBase
+    {
+        private readonly AbsenseApiManager _manager = new AbsenseApiManager();
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet]
+        public IEnumerable<Students> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet("{name}")]
+        public ActionResult<Students> Get(string name)
+        {
+            Students student = _manager.GetByName(name);
+            if (student == null) return NotFound("lmao");
+            return student;
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status405MethodNotAllowed)]
+        [HttpPost]
+        public ActionResult<Students> Post([FromBody] Students value)
+        {
+            Students students = _manager.Add(value);
+            return Created("/" + students.StudentId ,students);
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpPut("id")]
+        public ActionResult<Students> Put(int id, [FromBody] Students value)
+        {
+            Students student = _manager.Update(id, value);
+            if (student == null) return NotFound("There is no student with the given Id");
+            return Ok(value);
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpDelete("id")]
+        public ActionResult<Students> Delete(int id)
+        {
+            Students student = _manager.Delete(id);
+            if (student == null) return NotFound("There is no student with that id");
+            return Ok(student);
+            
+
+        }
+
+
+    }
+}
---
+++ SysUdvikling2022/AbsenseApi/AbsenseApi/Managers/AbsenseApiManager.cs
@@ -0,0 +1,57 @@
+﻿using StudentLibrary;
+
+namespace AbsenseApi.Managers
+{
+    public class AbsenseApiManager
+    {
+        public static int nextId = 1;
+        private static readonly List<Students> StudentsList = new List<Students>
+        {
+            new Students(studentId: nextId++,name: "Jonas", absenceMin: 24, nFCId: 12123434),
+            new Students(studentId: nextId++,name: "Anthon", absenceMin: 24, nFCId: 12123434),
+            new Students(studentId: nextId++,name: "Anton", absenceMin: 24, nFCId: 12123434),
+            new Students(studentId: nextId++,name: "Morten", absenceMin: 24, nFCId: 12123434),
+        };
+
+        public List<Students> GetAll()
+        {
+            List<Students> students = new List<Students>(StudentsList);
+            return students;
+        }
+
+        public Students GetById(int studentId)
+        {
+            return StudentsList.Find(student => student.StudentId == studentId);
+        }
+
+        public Students GetByName(string name)
+        {
+            return StudentsList.Find(student => student.Name == name);
+        }
+
+        public Students Add(Students newStudents)
+        {
+            newStudents.StudentId = nextId++;
+            StudentsList.Add(newStudents);
+            return newStudents;
+        }
+
+        public Students Delete(int studentId)
+        {
+           Students students = StudentsList.Find(student => student.StudentId == studentId);
+           if (students == null)return null;
+           StudentsList.Remove(students);
+           return students;
+        }
+
+        public Students Update(int studentId, Students update)
+        {
+            Students? students = StudentsList.Find(student => student.StudentId == studentId);
+            if (students == null) return null;
+            students.Name = update.Name;
+            students.AbsenceMin = update.AbsenceMin;
+            students.NFCId = update.NFCId;
+            return students;
+        }
+    }
+}
