9cef3f6bc6762a94ac64d9d07c66a943ddfa47bb
Zyhler <icyxgram@gmail.com>
2022-12-12 11:10:49 +0100

    Add files via upload
    Listener, som sender til RestAPI når du kører på din PC.

---
+++ Raspberry/UDPServerForAbsence.py
@@ -0,0 +1,39 @@
+from socket import *
+
+import requests
+import json
+import urllib3
+
+urllib3.disable_warnings();
+
+urlwithID = "";
+
+url = "https://localhost:7230/api/AbsenseApi/{}"
+
+
+serverPort = 7000
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind(('', serverPort))
+print("The server is ready to receive")
+while True:
+    message, clientAddress = serverSocket.recvfrom(2048)
+    modifiedMessage = message.decode()
+    parts = modifiedMessage.split('+')
+    studentId = parts[0]
+    urlWithID = url.format(studentId)
+    print(studentId)
+    print("MAC:"+parts[1])
+    #studentIdJson = json.loads(studentId)
+    #studentIdJson2 = studentId.json()
+    print(urlWithID)
+    response = requests.put(urlWithID, verify=False)
+    statuscode = response.status_code
+    if statuscode == 200:
+        print("Check in Succesfull")
+    else:
+        print("check in failed",statuscode)
+
+
+
+    #serverSocket.sendto(modifiedMessage.encode(),clientAddress)
+
